d779082be76c5a1342be0e268d110fe5ef46c70c
nirajsathe80 <nirajsathe80@gmail.com>
2024-06-22 11:28:55 +0530

    data file added

---
+++ Background-Color-Change/src/components/data/index.js
@@ -0,0 +1,49 @@
+const Colours = [
+  {
+    label: "Red",
+    value: "red",
+    textColor: "white",
+  },
+  {
+    label: "Pink",
+    value: "pink",
+    textColor: "red",
+  },
+  {
+    label: "Green",
+    value: "green",
+    textColor: "white",
+  },
+  {
+    label: "Blue",
+    value: "blue",
+    textColor: "white",
+  },
+  {
+    label: "Orange",
+    value: "orange",
+    textColor: "white",
+  },
+  {
+    label: "Gray",
+    value: "gray",
+    textColor: "blue",
+  },
+  {
+    label: "Black",
+    value: "black",
+    textColor: "white",
+  },
+  {
+    label: "Brown",
+    value: "brown",
+    textColor: "white",
+  },
+  {
+    label: "Aqua",
+    value: "aqua",
+    textColor: "red",
+  },
+];
+
+export default Colours;
